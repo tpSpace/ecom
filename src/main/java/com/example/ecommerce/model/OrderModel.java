d67b0e38ab40881a50af4d2e381e8c9aa605c64e
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-04-08 06:03:35 +0000

    feat: add quantity orders table

--- src/main/java/com/example/ecommerce/model/OrderModel.java
+++ src/main/java/com/example/ecommerce/model/OrderModel.java
@@ -4,10 +4,16 @@ import java.sql.Date;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "orders")
+@Data
 public class OrderModel {
     @Id
     @GeneratedValue
@@ -21,6 +27,9 @@ public class OrderModel {
     @OneToMany(mappedBy = "product")
     @Column(name = "product_id", nullable = false)
     private UUID productId;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
 
     @Column(name = "order_date", nullable = false)
     private Date orderDate;
--- src/main/java/com/example/ecommerce/model/ProductModel.java
+++ src/main/java/com/example/ecommerce/model/ProductModel.java
@@ -3,10 +3,16 @@ package com.example.ecommerce.model;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-public class productModel {
+@Entity
+@Table(name = "products")
+@Data
+public class ProductModel {
     @Id
     @GeneratedValue
     @Column(columnDefinition = "uuid", name = "id", updatable = false, nullable = false, unique = true)
